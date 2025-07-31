352e32d6ec7f1a4507cacfffc8722afee045fe28
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-07-31 17:32:28 +1000

    feat: add initial room implementation

--- apps/backend/src/socket.ts
+++ apps/backend/src/socket.ts
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
+import { Room } from "./room";
 
-export function setUpSocketListeners(io: Server) {
+export function setUpSocketListeners(io: Server, roomMap: Map<string, Room>) {
   io.on("connection", (socket) => {
     console.log("User connected", socket.id);
   });
