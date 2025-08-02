7a369215571e8e7382e4c330f18dd336a8923ef6
adrianbalbs <adrianbalbs@hotmail.com>
2025-08-02 11:22:08 +1000

    chore: refactor socket integration test setup

--- apps/backend/src/tests/socket.test.ts
+++ apps/backend/src/tests/socket.test.ts
@@ -1,27 +1,29 @@
 import { afterAll, beforeAll, describe, expect, test } from "vitest";
-import { io as Client, Socket } from "socket.io-client";
+import { io as ioc, Socket as ClientSocket } from "socket.io-client";
 import { httpServer } from "../server";
 
 describe("Socket Integration Tests", () => {
   const userId = "123";
   const name = "Adrian";
-  let clientSocket: Socket;
+  let clientSocket: ClientSocket;
 
   // We need to set up a test client socket and set up the http server to test
   beforeAll(async () => {
     const port = 3001;
-    await new Promise<void>((resolve) => httpServer.listen(port, resolve));
-    clientSocket = Client(`http://localhost:${port}`, {
-      query: { userId, name },
-    });
+    await new Promise<void>((resolve) =>
+      httpServer.listen(port, () => {
+        clientSocket = ioc(`http://localhost:${port}`, {
+          query: { userId, name },
+        });
+        clientSocket.on("connect", resolve);
+      }),
+    );
   });
 
   // Need to ensure the socket is not left open
   afterAll(async () => {
-    clientSocket.close();
-    await new Promise<void>((resolve, reject) =>
-      httpServer.close((err) => (err ? reject(err) : resolve())),
-    );
+    httpServer.close();
+    clientSocket.disconnect();
   });
 
   test("Test room creation", async () => {
