dc3ded21d493f815173be785a2a3db5343c091e0
Deki <deckee7@gmail.com>
2025-08-06 12:32:43 +1000

    setup routing for home, room, results pages

---
+++ apps/frontend/src/pages/RoomPage.tsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+export default function RoomPage() {
+  // IF HOST -> option to share link, play the game, Host control panel where you can see how many people completed + end vote early + timer of when it ends
+  // IF ATTENDEE -> play the game.
+  const { roomId } = useParams();
+
+  return <h1>Room Page — Room ID: {roomId}</h1>;
+}
