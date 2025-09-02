9064eaade7d28380554fa5d1f79b9c3105ef255b
adrianbalbs <adrianbalbs@hotmail.com>
2025-09-02 12:56:35 +1000

    chore: add some doc comments to useSocket

--- apps/frontend/src/contexts/socketContext.ts
+++ apps/frontend/src/contexts/socketContext.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 import type { Socket } from "socket.io-client";
 
-type SocketContextType = { socket: Socket | null };
+export type SocketContextType = { socket: Socket | null };
 export const SocketContext = createContext<SocketContextType>({ socket: null });
--- apps/frontend/src/contexts/useSocket.ts
+++ apps/frontend/src/contexts/useSocket.ts
@@ -1,7 +1,13 @@
 import { useContext } from "react";
-import { SocketContext } from "./socketContext";
+import { SocketContext, type SocketContextType } from "./socketContext";
 
-export const useSocket = () => {
+/**
+ * Exposes the socket.io connection to the component for defining event handling logic
+ * and emitting events.
+ *
+ * @throws {Error} if not called within a SocketProvider
+ */
+export const useSocket = (): SocketContextType => {
   const context = useContext(SocketContext);
   if (!context) {
     throw new Error("useSocket must be used within a SocketProvider");
