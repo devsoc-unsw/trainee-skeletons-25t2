9698edded01f95ff2f297fffadc3a7b7ce5dc47e
Lucia Jin <z5589595@ad.unsw.edu.au>
2025-09-03 14:33:14 +1000

    feat: implemented basic state and refined css

--- apps/frontend/src/pages/HomePage.tsx
+++ apps/frontend/src/pages/HomePage.tsx
@@ -1,40 +1,92 @@
+import { useState } from 'react';
 import "./Pages.css";
 
+type Mode = "host" | "join";
+
 export default function HomePage() {
+  // Page state
+  const [mode, setMode] = useState<Mode>("host");
+
+  // User input
+  const [name, setName] = useState("");
+  const [endTime, setTime] = useState("");
+  const [code, setCode] = useState("");
+
+  // Placeholder for submission
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (mode === "host") {
+      console.log("Hosting game:", { name, endTime });
+    } else {
+      console.log("Joining game:", { name, code });
+    }
+  };
+
+  // Returns page info
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-[520px] h-[560px] bg-gray-300/50 rounded-lg flex items-end justify-center p-8">
-        <div className="w-[456px] h-[440px] bg-gray-100 rounded-lg flex flex-col items-center justify-start p-2 gap-4">
+    <div className="flex-cc">
+      <div className="w-[520px] h-[560px] panel-back">
+        <div className="w-[456px] h-[440px] panel-main">
           <img
             src='nomnomvotes_logo.png'
             alt='cookie'
             className="w-40 h-40 object-contain"
           />
 
-          <div className="w-[400px] h-[32px] flex items-center justify-center px-2 gap-8">
-            <div className='w-[180px] h-[32px] bg-gray-300 rounded-lg'>
-              <p className="text-gray-900 text-center">Host Game</p>
-            </div>
-            <div className='w-[180px] h-[32px] bg-gray-300 rounded-lg'>
-              <p className='text-gray-900 text-center'>Join Game</p>
-            </div>
+          <div className="w-[400px] h-[32px] flex-cc px-2 gap-8">
+            <button
+              type="button"
+              onClick={() => setMode("host")}
+              className="w-[180px] h-[32px] button-grey">Host Game
+            </button>
+            <button
+              type="button"
+              onClick={() => setMode("join")}
+              className="w-[180px] h-[32px] button-grey">Join Game
+            </button>
           </div>
 
           <div className="w-[400px] flex flex-col gap-1">
             <i className="text-gray-900 text-left">Your name</i>
-            <div className="h-[32px] border border-blue-200 rounded-lg">
-              <p className="text-blue-200 px-[8px]">Enter name</p>
-            </div>
+            <input
+              type="text"
+              placeholder="Enter name"
+              className="h-[32px] input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}>
+            </input>
 
-            <i className="text-gray-900 text-left">Start time</i>
-            <div className="h-[32px] border border-blue-200 rounded-lg">
-              <p className="text-blue-200 px-[8px]">Enter name</p>
-            </div>
+            <i className="text-gray-900 text-left">{mode === "host" ? "End time" : "Room code"}</i>
+            {mode === "host" && (
+              <input
+                type="datetime-local"
+                placeholder="Voting end time"
+                className="h-[32px] input-box"
+                value={endTime}
+                onChange={(e) => setTime(e.target.value)}>
+              </input>
+            )}
+
+            {mode === "join" && (
+              <input
+                type="text"
+                placeholder="Room code"
+                className="h-[32px] input-box"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}>
+              </input>
+            )}
+
           </div>
 
-          <div className='w-[400px] h-[40px] bg-indigo-400 rounded-lg text-center'>
-            <p>Create Game</p>
-          </div>
+          <button
+            type='button'
+            onClick={handleSubmit}
+            className="w-[400px] h-[40px] button-blue text-center flex-cc"
+          >
+            {mode === "host" ? "Create Game" : "Join Game"}
+          </button>
         </div>
       </div>
     </div>
